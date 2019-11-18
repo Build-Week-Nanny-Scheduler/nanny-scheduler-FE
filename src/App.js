ce4d771f4bce609d0be9021899cd8f7bb505a6dc
newbi462 <isthisemailfree@gmail.com>
2019-11-18 12:49:30 -0500

    Typo fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">App</div>;
+
+  return (
+    <div className="App">App</div>
+
+    {/*SCRATCH LINKS SO I CAN WORK ON AND SEE WHAT I AM BUILDING*/}
+    
+
+  );
 }
 
 export default App;
