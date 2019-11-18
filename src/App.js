3fe21d6446cd448ef4de654add7634ac6233245c
newbi462 <isthisemailfree@gmail.com>
2019-11-18 13:57:43 -0500

    Login Form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 
 import { Route, Link } from 'react-router-dom';
 
-import { RegisterFromFormik } from './components/Register';
+import { RegisterFromFormik } from './components/auth/Register';
+import { LoginFromFormik } from './components/auth/Login';
 
 function App() {
 
@@ -12,8 +13,10 @@ function App() {
 
     {/*SCRATCH LINKS SO I CAN WORK ON AND SEE WHAT I AM BUILDING*/}
     <Link to="/register">Register</Link>
+    <Link to="/login">Login</Link>
 
     <Route path="/register" component={RegisterFromFormik} />
+    <Route path="/login" component={LoginFromFormik} />
 
     </div>
 
