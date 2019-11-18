666fb22f914385a75454cace2fe9a1f7e1dfd24b
newbi462 <isthisemailfree@gmail.com>
2019-11-18 17:23:07 -0500

    Start of nav bar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { Route, Link } from "react-router-dom";
 import { RegisterFromFormik } from "./components/auth/Register";
 import { LoginFromFormik } from "./components/auth/Login";
 import Home from "./components/layout/Home";
-import CreateRequest from "./components/requests/CreateRequest";
+import { CreateRequest } from "./components/requests/CreateRequest";
+import { Navigation } from "./components/navigation/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <Link to="/menu">
+        <div className="topBanor">
+          Nanny in a Flash
+        </div>
+      </Link>
       App
+      <Route exact path="/menu" component={Navigation} />
       <Route exact path="/" component={Home} />
       <Route path="/" component={CreateRequest} />
       <Route path="/register" component={RegisterFromFormik} />
