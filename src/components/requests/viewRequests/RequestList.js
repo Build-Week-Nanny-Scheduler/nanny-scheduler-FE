62ee9928af6a1a8ff4c5aa49fb7f3f4fe9020f14
Anthony <anthony.kizer88@gmail.com>
2019-11-19 15:04:55 -0800

    added dummy data to RequestList.js

--- src/components/requests/viewRequests/RequestList.js
+++ src/components/requests/viewRequests/RequestList.js
@@ -3,10 +3,48 @@ import { axiosWithAuth } from "axios";
 import { RequestContext } from "../../../contexts/requestContext";
 
 const RequestList = () => {
-  const [requestList, setRequestList] = useContext(RequestContext);
-  //NannyContext will set nannylist to whatever was returned from your axiosWithAuthCall
-  console.log(requestList);
-  return <div></div>;
+  // const [requestList, setRequestList] = useContext(RequestContext);
+
+  const [requestList, setRequestList] = useState([
+    {
+      name: "Mary Jenkins",
+      city: "San Francisco",
+      state: "California",
+      numberOfKids: "3",
+      kidsAges: "1, 3, 5",
+      timeNeeded: "5:30pm - 8pm monday, wednesday"
+    },
+    {
+      name: "Bob Barker",
+      city: "San Francisco",
+      state: "California",
+      numberOfKids: "3",
+      kidsAges: "2, 5, 8",
+      timeNeeded: "4:30pm - 8pm monday - friday"
+    },
+    {
+      name: "Chu Lor",
+      city: "Chicago",
+      state: "Illinois",
+      numberOfKids: "1",
+      kidsAges: "3",
+      timeNeeded: "5:30pm - 9pm monday - friday"
+    },
+    {
+      name: "Robert Robertson",
+      city: "Chicago",
+      state: "Illinois",
+      numberOfKids: "5",
+      kidsAges: "3, 5, 7, 8, 9",
+      timeNeeded: "2:30pm - 9pm monday - friday"
+    }
+  ]);
+
+  return (
+    <div>
+      <h1>Current Listings</h1>
+    </div>
+  );
 };
 
 export default RequestList;
