f78d8a4138a7f3fabee641b82ae0465b14841c30
newbi462 <isthisemailfree@gmail.com>
2019-11-19 18:41:29 -0500

    Cards up with dummy data

--- src/components/requests/viewRequests/RequestList.js
+++ src/components/requests/viewRequests/RequestList.js
@@ -42,7 +42,19 @@ const RequestList = () => {
 
   return (
     <div>
-      <h1>Current Listings</h1>
+      {requestList.map(item => (
+        <div key={item} className="nannyCard">
+          <h2>
+            {item.name} needs a Nanny
+          </h2>
+          <div key={item.id} className="card2Grid">
+            <div>Number Of Kids:</div><div>{item.numberOfKids ? item.numberOfKids : "Ask Me"}</div>
+            <div>Kids Ages:</div><div>{item.kidsAges ? item.kidsAges : "Ask Me"}</div>
+            <div>Location:</div><div>{item.city ? item.city : "Ask Me"}, {item.state ? item.state : "Ask Me"}</div>
+            <div>Time Needed:</div><div>{item.timeNeeded ? item.timeNeeded : "Ask Me"}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
