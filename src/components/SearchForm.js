2080896a5eee9723cb6b47beb6dcf0260ac661ae
Anthony <anthony.kizer88@gmail.com>
2019-11-22 14:17:17 -0800

    fixed bug in search function

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,35 +2,35 @@ import React, { useEffect, useState } from "react";
 //import axios from "axios";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-export default function SearchForm( props ) {
+export default function SearchForm(props) {
   const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     axiosWithAuth()
-    .get(`${props.slash}`)
-    .then(response => {
-      console.log(response.data)
-      const killsearch = "";
+      .get(`${props.slash}`)
+      .then(response => {
+        const nannyList = response.data.filter(nanny => nanny.isNanny);
+        const killsearch = "";
 
-      const inSearchBar = response.data.filter(item =>
-        (item.city != null) ? item.city.toLowerCase().includes(searchValue.toLowerCase()) : killsearch.includes(searchValue.toLowerCase())
-      );
-      props.setState(inSearchBar);
-
-    })
-    .catch( error => {
-      console.log("error", error.message)
-    })
-  },[searchValue])
+        const inSearchBar = nannyList.filter(item =>
+          item.isNanny
+            ? item.city.toLowerCase().includes(searchValue.toLowerCase())
+            : killsearch.includes(searchValue.toLowerCase())
+        );
+        props.setState(inSearchBar);
+      })
+      .catch(error => {
+        console.log("error", error.message);
+      });
+  }, [searchValue]);
 
   const newLetterEntered = event => {
     setSearchValue(event.target.value);
   };
 
-
   return (
     <section className="search-form">
-    {/*// Add a search form here*/}
+      {/*// Add a search form here*/}
       <form className="search">
         <input
           type="text"
@@ -43,7 +43,6 @@ export default function SearchForm( props ) {
           autoComplete="off"
         />
       </form>
-
     </section>
   );
 }
